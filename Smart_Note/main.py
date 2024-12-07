d928e122bda166aa21dac341a0e12ae8bc4be2f5
TMykola <milolatarasuk@gmail.com>
2024-12-07 13:46:34 +0200

    Update main.py

--- Smart_Note/main.py
+++ Smart_Note/main.py
@@ -14,12 +14,34 @@ def error(text):
 def show_note():
     note_list_widget.addItems(list(data.keys()))
 
+def show_tag(name_note):
+    tag_list_widget.addItems(data[name_note]["TAG"])
 
 def add_note():
     name_note = QInputDialog().getText(QInputDialog(), "Назва нотатка:", "Введіть назву нотатка:")[0]
-    data[name_note] = {"TEXT": "", "TAG": {}}
+    data[name_note] = {"TEXT": "", "TAG": []}
     write_json(name_file, data)
     note_list_widget.addItem(name_note)
+
+def add_tag():
+    try:
+        name_file = note_list_widget.currentItem().text()
+        name_tag = QInputDialog().getText(QInputDialog(), "Введення тега:", "Введіть назву тега:")[0]
+        data[name_file]["TAG"].append(name_tag)
+        write_json(name_file, data)
+        tag_list_widget.addItem(name_tag)
+    except:
+        error("Щоб створити тег, спочатку виберіть нотаток")
+def remove_tag():
+    try:
+        name_note = note_list_widget.currentItem().text()
+        name_tag = tag_list_widget.currentItem().text()
+        data[name_note]["TAG"].remove(name_tag)
+        write_json(name_note, data)
+        tag_list_widget.removeItemWidget(name_tag)
+    except:
+        error("Щоб видаляти тег, спочатку виберіт нотаток а потім тег")
+
 
 def save_file():
     try:
@@ -32,15 +54,25 @@ def save_file():
 def show_text():
     name_note = note_list_widget.currentItem().text()
     text_edit.setPlainText(data[name_note]["TEXT"])
+    text_edit.setDisabled(False)
+    show_tag(name_note)
 
 def remove_note():
     try:
+        
         name_note = note_list_widget.currentItem().text()
         del data[name_note]
+        write_json(name_file, data)
+        note_list_widget.clear()
+        show_note()
+        text_edit.setPlainText("")
+        text_edit.setDisabled(True)
     except:
         error("Щоб видалити, спочатку виберіть нотаток!")
 
 
+delete_tag.clicked.connect(remove_tag)
+create_tag.clicked.connect(add_tag)
 delete_note.clicked.connect(remove_note)
 create_note.clicked.connect(add_note)
 save_note.clicked.connect(save_file)
